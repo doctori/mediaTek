2bbd0205eab15667773917782c389b730f44cbc6
doctori <lerouge.pierre@gmail.com>
2015-02-23 21:01:59 +0000

    middle forms validation

--- records/models.py
+++ records/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+class Artist(models.Model):
+	name = models.TextField(default='')
+	class Meta:
+		ordering = ('id',)
+	def __str__(self):
+		return self.name
+	def get_absolute_url(self):
+		return reverse('view_artist',args=[self.id])
+		
+class Record(models.Model):
+	name = models.TextField(default='')
+	artist = models.ForeignKey(Artist,default=None)
+	
+	class Meta:
+		ordering = ('id',)
+		unique_together = ('artist','name')
+	def __str__(self):
+		return self.name
+	
+	def get_absolute_url(self):
+		return reverse('view_record',args=[self.id])
--- records/views.py
+++ records/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.core.exceptions import ValidationError
+from records.forms import (
+	RecordForm, EMPTY_ITEM_ERROR,
+	DUPLICATE_ITEM_ERROR,ExistingRecordItemForm
+	)
+from records.models import Record, Artist
 
-# Create your views here.
+def home_page(request):
+	return render(request, 'home.html',{'form':RecordForm()})
+
+def view_record(request,record_id):
+	#We retrieve the list object from the URL
+	record = Record.objects.get(id=list_id)
+	form = ExistingRecordItemForm(for_record=record)
+	if request.method == 'POST':
+		form = ExistingRecordItemForm(for_record=record,data=request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect(list_)
+	return render(request, 'record.html',{'record':record, 'form':form})
+
+def new_record(request):
+	form = RecordForm(data=request.POST)
+	if form.is_valid():
+		record = Record.objects.create()
+		form.save(for_record=record)
+		return redirect(record)
+	else:
+		return render(request, 'home.html',{"form":form})
