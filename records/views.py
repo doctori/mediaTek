34c29bd490d919af47763e4d976e9c596b4fbafe
doctori <lerouge.pierre@gmail.com>
2015-04-02 20:10:05 +0000

    test U OK on artists display and minimal form for popup form

--- records/views.py
+++ records/views.py
@@ -8,20 +8,23 @@ from records.models import Record, Artist
 
 def home_page(request):
 	artists = Artist.objects.all()
-	return render(request, 'home.html',{'artists':artists,'minimalRecordForm':MinimalRecordForm()})
+	minimalForm=MinimalRecordForm()
+	return render(request, 'home.html',{'artists':artists,'minimalRecordForm':minimalForm})
 
 def view_record(request,record_id):
 	#We retrieve the record object from the URL
 	record = get_object_or_404(Record,id=record_id)
+	minimalForm=MinimalRecordForm()
 	#we retrieve the artist ID from the POST
 	form = ExistingArtistRecordForm(request.POST or None,instance=record)
 	if form.is_valid():
 		form.save()
 		return redirect(record)
-	return render(request, 'record.html',{'record':record, 'form':form,'minimalRecordForm':MinimalRecordForm()})
+	return render(request, 'record.html',{'record':record, 'form':form,'minimalRecordForm':minimalForm})
 
 def new_record(request):
 	form = RecordForm(data=request.POST)
+	minimalForm=MinimalRecordForm()
 	#we retrieve the artist ID from the POST
 	if form.is_valid():
 		#record = Record.objects.create()
@@ -29,11 +32,12 @@ def new_record(request):
 
 		return redirect(record)
 	else:
-		return render(request, 'home.html',{"form":form})
+		return render(request, 'home.html',{"form":form,'minimalRecordForm':minimalForm})
 def view_artist(request,artist_id):
 	artist = get_object_or_404(Artist,id=artist_id)
+	minimalForm=MinimalRecordForm()
 	form = ArtistForm(request.POST or None,instance=artist)
 	if form.is_valid():
 		form.save()
 		return redirect(artist)
-	return render(request, 'artist.html',{'artist':artist,'form':form,'minimalRecordForm':MinimalRecordForm()})
+	return render(request, 'artist.html',{'artist':artist,'form':form,'minimalRecordForm':minimalForm})
