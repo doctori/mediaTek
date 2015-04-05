b7fc91bdf207e614317edfeaf7bbfe6d2df5bea0
doctori <lerouge.pierre@gmail.com>
2015-04-05 02:23:47 +0200

    almost gunicorn/supervisor/nginx workgin

---
+++ deploy_tools/start-gunicorn.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+NAME="mediaTek"                               # Name of the application
+SERVER="dev.lerouge.info"
+DJANGODIR=/app             # Django project directory
+SOCKFILE=/var/gunicorn/$SERVER  # we will communicate using this unix socket
+USER=mediaTek
+GROUP=$USER
+NUM_WORKERS=4                                     # how many worker processes should Gunicorn spawn
+DJANGO_WSGI_MODULE=mediaTek.wsgi                     # WSGI module nameœ
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+LOGDIR=/var/log/gunicorn
+test -d $LOGDIR || mkdir -p $LOGDIR
+chown $USER:$GROUP $LOGDIR
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec /usr/local/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --bind=unix:$SOCKFILE \
+  --log-level=debug \
+  --error-logfile=${LOGDIR}/error-$NAME.log \
+  --access-LOGDIR=${logfile}/acces-$NAME.log
+
--- deploy_tools/start-srv.sh
+++ deploy_tools/start-srv.sh
@@ -1,4 +1,4 @@
 echo "starting django but waiting 15 sec in order to wait for the DB"
 sleep 15;
 python3.2 manage.py migrate --noinput
-python3.2 manage.py runserver 0.0.0.0:8000
+ supervisord -c /etc/supervisor/supervisord.conf -n
